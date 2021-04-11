fe401cef5129933b88c7f50dc538ea79ad6c7ae3
Jordan <jordan.barrett.jm@gmail.com>
2021-04-11 21:10:38 +0000

    added ordering by name for the produce on the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < ApplicationController
   		redirect_to new_user_session
   	end
 
-  	@produces = Produce.where(available: true)
-  	@produces = Produce.where("name ILIKE ?", "%#{params[:search][:name]}%") if params[:search].present?
+  	@produces = Produce.where(available: true).order(:name)
+  	@produces = Produce.where("name ILIKE ?", "%#{params[:search][:name]}%").order(:name) if params[:search].present?
   end
 
   def landing
